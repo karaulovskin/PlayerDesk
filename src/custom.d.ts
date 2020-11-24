0723fcffac2eae11377d50ce0aec8f20eee1ad64
karaulovskin <karaulovskin@yandex.ru>
2020-11-24 21:25:25 +0300

    add @svgr/webpack && url-loader

---
+++ src/custom.d.ts
@@ -0,0 +1,7 @@
+declare module '*svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGAElement>>;
+  const src: string;
+  export default src;
+}
